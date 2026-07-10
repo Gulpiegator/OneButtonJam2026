48159b3d889b1034b0f1b67595fd39fe657f487c
NZig618 <94143570+NZig618@users.noreply.github.com>
2026-07-09 22:41:00 -0500

    Initial Scene Manager and Title Screen
    Added input handling and (basic) scene transitions to GameController. Also built out a Title Screen.

--- The Wizard Code/Assets/Scripts/GameController.cs
+++ The Wizard Code/Assets/Scripts/GameController.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System;
 
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
     public Sprite[] runesSprites;
+
+    #region Input Managment
+    public static CharacterController Controller;
+    private bool input;
+    #endregion
+
+    #region Scene Manager
+    public bool sceneChangeEnabled;
+    public string nextSceneName;
+    #endregion
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instance = this;
+        Controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sceneChangeEnabled) {
+
+            // Check if the Spacebar was pressed down during this frame
+            if (input) 
+            { 
+                LoadNextScene(); 
+            } 
+        }
+    }
+
+    public void OnPressSpacebar(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<float>() > 0.5f;
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
